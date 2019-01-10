c9bba9bba78d8ad106584d63cf12b83ea6a60b2e
Shawn Mullings <shawnmullings@gmail.com>
2019-01-10 09:20:10 -0500

    updated README.md

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Threading;
 
-namespace Threading
+namespace Tenna
 {
-    class MainClass
+    internal class ThreadingSync
     {
-        public static void Main(string[] args)
+        private static EventWaitHandle onReadyEvenThread;
+        private static EventWaitHandle onReadyOddThread;
+
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            onReadyOddThread = new AutoResetEvent(true);
+            onReadyEvenThread = new AutoResetEvent(false);
+
+            // Create individul threads
+            Thread threadOne = new Thread(oddNumbers);
+            Thread threadTwo = new Thread(evenNumbers);
+
+            //Start threads
+            threadOne.Start();
+            threadTwo.Start();
+            
+            threadOne.Join();
+            threadTwo.Join();
+        }
+
+        // Method call by 'threadOne' to write odd numbers to console
+        public static void oddNumbers()
+        {
+            for (int i = 1; i < 100; i += 2)
+            {
+                onReadyEvenThread.WaitOne();
+                Console.WriteLine("thread " + 1 + ": The number is '" + i + "'");
+                onReadyOddThread.Set();
+            }
+        }
+
+        // Method call by 'threadTwo' to write even numbers to console
+        public static void evenNumbers()
+        {
+            for (int i = 0; i <= 100; i += 2)
+            {
+                onReadyOddThread.WaitOne();
+                if (i > 0)
+                    Console.WriteLine("thread " + 2 + ": The number is '" + i + "'");
+                onReadyEvenThread.Set();
+            }
         }
     }
 }
